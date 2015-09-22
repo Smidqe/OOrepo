92a9ce9135fd45a1c73aae988e34287ba7c58143
Miikka Lahtinen <smidqe@hotmail.fi>
2015-09-22 08:08:35 +0300

    Something has changed

--- H02/src/main/Dog.java
+++ H02/src/main/Dog.java
@@ -2,12 +2,28 @@ package main;
 
 public class Dog {
 	String name;
+	int age;
 	
 	public Dog(String s) {
 		name = s;
 	}
 	
-	public void speak() {
+	public String getName() {
+		return name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void bark() {
+		System.out.println("Hau!");
+	}
+	
+	public void speak(String s) {
 		System.out.println("Hei! Minun nimeni on " + name);
+		
+		if (!s.isEmpty())
+			System.out.println(s);
 	}
 }
--- H02/src/main/Mainclass.java
+++ H02/src/main/Mainclass.java
@@ -1,11 +1,27 @@
 package main;
 import main.Dog;
+//import java.awt.*;
 
 public class Mainclass {
     public static void main(String[] args) {
     	Dog dog = new Dog("Wuff");
-
+    	//hello
+    	TPoint p = new TPoint(5, 5);
     	
-    	dog.speak();
+    	p.print();
+    	p.offset(5);
+    	p.print();
+    	p.offset(-5);
+    	p.print();
+    	
+    	p.offset(-5, 5);
+    	p.print();
+    	
+    	System.out.println(p.distance(new TPoint(4, 4)));
+    	
+    	p.pos();
+    	p.print();
+    	
+    	dog.speak("hello");
     }
 }
