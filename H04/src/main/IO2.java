b6c911dd24cc24a083e771627a8c9b78c95b16f7
Miikka Lahtinen <smidqe@hotmail.fi>
2015-10-05 15:38:09 +0300

    Finished H04

--- H04/src/main/IO2.java
+++ H04/src/main/IO2.java
@@ -1,9 +1,11 @@
 package main;
 
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 import java.io.*;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.zip.*;
 
 class TFile {
 	
@@ -80,6 +82,8 @@ public class IO2 {
 	        while ((c = r.read()) != -1) 
 	            res.append((char) c);
 	        
+	        r.close();
+	        
 		} catch (IOException e) {
 	        throw new RuntimeException(e);
 		}
@@ -89,26 +93,94 @@ public class IO2 {
 		return res.toString();
 	}
 	
-	public boolean write(String path, String text) throws IOException//(file, )
+	public boolean write(String path, String text, boolean append) throws IOException//(file, )
 	{
 		
 		OutputStreamWriter r = null;
 		try {
-			r = new OutputStreamWriter(new FileOutputStream(path), "UTF-8");
+			r = new OutputStreamWriter(new FileOutputStream(path, append), "UTF-8");
 			r.write(text);
+
+			r.close();
 			
 		} catch (Exception e) {
-			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return true;
+	}
+	
+	public void copy(String from, String to) throws IOException
+	{	
+		try{
+			write(to, read(from), false);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return;
+	}
+	
+	public boolean copy(String from, String to, int maxSize) throws IOException
+	{
+		String s = "";
+		String a[];
+		StringBuilder r = new StringBuilder();
+
+		try {
+			s = read(from);
+			a = s.split("\\R");
+			
+			int i;			
+			for (i = 0; i < a.length; i++)
+			{
+				if (a[i].length() == 0)
+					continue;
+				
+				if ((a[i].trim().isEmpty()))
+					continue;
+				
+				r.setLength(0);
+				r.append(a[i]);
+				if (a[i].length() < maxSize)
+				{
+					if (a[i].contains("v"))
+						write(to, r.append(System.getProperty("line.separator")).toString(), true);
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		
 		return true;
 	}
 	
-	public boolean write(String path, String text, int maxSize)
+	public void readZip(String name) throws IOException
 	{
-		
-		
-		return true;
+		try {
+			ZipFile zipf = new ZipFile(name);
+			FileInputStream file = new FileInputStream(name);
+			ZipInputStream zip = new ZipInputStream(new BufferedInputStream(file));
+			
+			ZipEntry entry;
+			
+			while ((entry = zip.getNextEntry()) != null)
+			{
+				BufferedReader br = new BufferedReader(new InputStreamReader(zipf.getInputStream(entry), "UTF-8"));			
+				
+				String s;
+				while ((s = br.readLine()) != null)
+					System.out.println(s);
+				
+			}
+			
+			zip.close();
+			zipf.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
-	
 }
--- H04/src/main/mainclass.java
+++ H04/src/main/mainclass.java
@@ -5,26 +5,14 @@ import java.io.IOException;
 public class mainclass {
 	public static void main(String[] args) {
 		IO2 file = new IO2();
-		
-		String s = "";
-		
+
 		try
 		{
-			s = file.read("input.txt");
+			file.readZip("zipinput.zip");
 		}
 			catch (IOException e)
 		{
 			e.printStackTrace();
-		}
-		
-		
-		if (!s.isEmpty())
-			System.out.println(s);
-		
-		try {
-			file.write("output.txt", s);
-		} catch (IOException e) {
-			// TODO: handle exception
 		}
 		
 	}
